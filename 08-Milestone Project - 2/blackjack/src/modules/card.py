a324c8475e0e5d73bffdc51a4774864070b6dbb0
DJEDOUR Rafik <rafik.djedour@loreal.com>
2019-07-30 21:26:22 +0200

    Spliting player.py into 2 modules player and card

---
+++ 08-Milestone Project - 2/blackjack/src/modules/card.py
@@ -0,0 +1,36 @@
+import random
+
+
+class Card():
+    def __init__(self, color="", value="", points=0):
+        self.color = color
+        self.value = value
+        self.points = points
+
+    def __str__(self):
+        """
+        print method of a card return the card value, its color and the number of points
+        """
+        return "Card color : "+self.color+"\nCard value : "+self.value+"\nNumber of points for this card : "+str(self.points)
+
+    # This is Class attributes that define available parameters for a card
+    colors_value = ["Carré", "Coeur", "Trèfle", "Pique"]
+
+    cards_value = {str(i): i for i in range(2, 10)}
+    # Points values
+    cards_value.update({i: 10 for i in ['Valet', 'Dame', 'Roi']})
+    cards_value.update({'As': (1, 10)})
+
+    @classmethod
+    def random_card(cls):
+        """
+        Method to select a random card
+        Output : Card object with random color, value and related number of
+        points
+        """
+        selected_card_value = random.choice(list(cls.cards_value.keys()))
+        selected_card_points = cls.cards_value[selected_card_value]
+        cls.color = random.choice(cls.colors_value)
+        cls.value = selected_card_value
+        cls.points = selected_card_points
+        return cls
--- 08-Milestone Project - 2/blackjack/src/modules/player.py
+++ 08-Milestone Project - 2/blackjack/src/modules/player.py
@@ -1,44 +1,19 @@
-import random
-
 # Import config
 balance_by_default = 17000
 playername_by_default = "Croupier"
 
 
-class Card():
-    def __init__(self, color="", value="", points=0):
-        self.color = color
-        self.value = value
-        self.points = points
-
-    def __str__(self):
-        return "Card color : "+self.color+"\nCard value : "+self.value+"\nNumber of points for this card : "+str(self.points)
-
-    # This is Class attributes that define available parameters for a card
-    colors_value = ["Carré", "Coeur", "Trèfle", "Pique"]
-
-    cards_value = {str(i): i for i in range(2, 10)}
-    # Points values
-    cards_value.update({i: 10 for i in ['Valet', 'Dame', 'Roi']})
-    cards_value.update({'As': (1, 10)})
-
-    @classmethod
-    def random_card(cls):
-        """
-        Method to select a random card
-        Output : Card object with random color, value and related number of
-        points
-        """
-        selected_card_value = random.choice(list(cls.cards_value.keys()))
-        selected_card_points = cls.cards_value[selected_card_value]
-        cls.color = random.choice(cls.colors_value)
-        cls.value = selected_card_value
-        cls.points = selected_card_points
-        return cls
-
-
 class Player():
     def __init__(self, name=playername_by_default, balance=balance_by_default, game=[]):
+        """
+        - if the name is empty or not provided, the player name should by the
+        name by default
+        - the balance value for the player by default is the balance value by
+        default
+        - if the balance value is not forced, it should take the balance value
+        by default divided by the maximum number of rounds (17)
+        - the initial game of a player, including the crouper, is empty
+        """
         if name:
             self.name = name
         else:
@@ -49,10 +24,28 @@ class Player():
         self.game = game
 
     def __len__(self):
+        """
+        len method return the number of cards in a player game
+        """
         return len(self.game)
 
     def __str__(self):
+        """
+        print method retunr the name, the balance and all the game of a player
+        """
         return "Player name : "+self.name+"\nPlayer balance : "+str(self.balance)+"\nPlayer cards : "+str(self.game)
-    
+
     def add_card_to_game(self):
+        """
+        Add a random card to the player game
+        """
         self.game.append(Card.random_card())
+
+    def game_value(self):
+        game_value = 0
+        for card in self.game:
+            if card.value = "As":
+                pass
+            else:
+                pass
+        return game_value
