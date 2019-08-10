f330a123deb464297cc4931637e6d8c0355ce5c6
rafdj <rafdj2002@hotmail.com>
2019-08-10 19:18:14 +0200

    Merging workflows and utils

--- 08-Milestone Project - 2/blackjack/src/main.py
+++ 08-Milestone Project - 2/blackjack/src/main.py
@@ -4,6 +4,6 @@ import os
 os.path.abspath(os.curdir)+"\\Complete-Python-3-Bootcamp\\08-Milestone Project - 2\\blackjack\\src"
 sys.path.append(os.path.abspath(os.curdir)+"\\Complete-Python-3-Bootcamp\\08-Milestone Project - 2\\blackjack\\src")
 
-from workflows.workflow_players_init import *
+from utils.players_init import *
 
 players_init()
