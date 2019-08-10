b1efbc8517e0fb62a345e6722f24758e3f2fbfd9
rafdj <rafdj2002@hotmail.com>
2019-08-10 23:43:03 +0200

    Init of there_is_a_winner function

--- 08-Milestone Project - 2/blackjack/src/main.py
+++ 08-Milestone Project - 2/blackjack/src/main.py
@@ -5,11 +5,11 @@ os.path.abspath(os.curdir)+"\\Complete-Python-3-Bootcamp\\08-Milestone Project -
 sys.path.append(os.path.abspath(os.curdir)+"\\Complete-Python-3-Bootcamp\\08-Milestone Project - 2\\blackjack\\src")
 
 from utils.players_init import *
+from utils.winner_test import *
 
 players = players_init()
 
-there_is_a_winner = False
-while not there_is_a_winner:
+while not there_is_a_winner(players)[1]:
     # Croupier is the player 0 he never add cards to himself
     for player in players[1:]:
         while True: 
@@ -24,5 +24,3 @@ while not there_is_a_winner:
                 break
             else:
                 print("I did not understand your answer, please select Y(yes) or N(no)")
-    there_is_a_winner = True
-    #TODO : Add the real test of winner 
--- 08-Milestone Project - 2/blackjack/src/utils/winner_test.py
+++ 08-Milestone Project - 2/blackjack/src/utils/winner_test.py
@@ -1,2 +1,12 @@
 def there_is_a_winner(players_list):
-    return players_list[0], True
+    """
+    Function to test if there is a winner in the game
+    Inputs : List of Player
+    Outputs :
+        the_winner : winner Player
+        there_is_a_winnner : Boolean
+    """
+    #TODO : Define the real winning rule
+    the_winner = players_list[0]
+    there_is_a_winner = len(players_list[1])>4
+    return the_winner, there_is_a_winner
