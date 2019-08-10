4634f55131d31c0d7578f3269487b7104700e4b3
rafdj <rafdj2002@hotmail.com>
2019-08-10 19:49:26 +0200

    Adding cards to croupier when a player says yes

--- 08-Milestone Project - 2/blackjack/src/main.py
+++ 08-Milestone Project - 2/blackjack/src/main.py
@@ -10,12 +10,15 @@ players = players_init()
 
 there_is_a_winner = False
 while not there_is_a_winner:
+    # Croupier is the player 0 he never add cards to himself
     for player in players[1:]:
         while True: 
             request_for_player = player.name + ", Do you want a card ? Y/N : "
             do_you_want_a_card = input(request_for_player)
             if do_you_want_a_card.strip().upper() == 'Y':
                 player.game.append(Card.random_card())
+                # Croupier add a card only if a player do it
+                player[0].game.append(Card.random_card())
                 break
             elif do_you_want_a_card.strip().upper() == 'N':
                 break
