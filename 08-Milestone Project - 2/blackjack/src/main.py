6bb3e3f6eb2a54f90dd620905fc3ce542e0e1c16
rafdj <rafdj2002@hotmail.com>
2019-08-10 18:25:52 +0200

    Adding __init__ file in utils to detect modules

--- 08-Milestone Project - 2/blackjack/src/main.py
+++ 08-Milestone Project - 2/blackjack/src/main.py
@@ -1,5 +1,14 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.curdir)+"\\Complete-Python-3-Bootcamp\\08-Milestone Project - 2\\blackjack\\src")
+
+from utils.card import *
+
 colors_value = ["Carré", "Coeur", "Trèfle", "Pique"]
 
 cards_value = {str(i): i for i in range(1, 10)}
 cards_value.update({i: 10 for i in ['Valet', 'Dame', 'Roi']})
 cards_value.update({'As': (1, 10)})
+
+test_card = Card.random_card()
