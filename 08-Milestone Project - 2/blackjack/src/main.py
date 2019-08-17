894ca2fc923262abf575752f443a48b9142a1163
rafdj <rafdj2002@hotmail.com>
2019-08-17 12:34:58 +0200

    First release of main and update set_init comments

--- 08-Milestone Project - 2/blackjack/src/main.py
+++ 08-Milestone Project - 2/blackjack/src/main.py
@@ -4,11 +4,25 @@ import os
 os.path.abspath(os.curdir)+"\\Complete-Python-3-Bootcamp\\08-Milestone Project - 2\\blackjack\\src"
 sys.path.append(os.path.abspath(os.curdir)+"\\Complete-Python-3-Bootcamp\\08-Milestone Project - 2\\blackjack\\src")
 
+from utils.card import *
+from utils.player import *
 from utils.players_init import *
-from utils.winner_test import *
+from utils.set_playing import *
+from utils.set_ending import *
 
 players = players_init()
 
+while True:
+    set_init(players)
+    if max([player.mise for player in players[1:]]) == 0:
+        break
+    else:
+        set_playing(players)
+        set_ending(players)
+        if max([player.game_value() for player in players[1:]]) == 0:
+            print("Finished : The bank has won ! All the players have lost")
+
+"""
 while not there_is_a_winner(players)[1]:
     # Croupier is the player 0 he never add cards to himself
     for player in players[1:]:
@@ -24,3 +38,4 @@ while not there_is_a_winner(players)[1]:
                 break
             else:
                 print("I did not understand your answer, please select Y(yes) or N(no)")
+"""
