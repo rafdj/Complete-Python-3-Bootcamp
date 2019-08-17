f87bf34869c9de57adaa182117b0f2433a18232a
rafdj <rafdj2002@hotmail.com>
2019-08-17 12:17:22 +0200

    Rename playing_oneset and winner_test

---
+++ 08-Milestone Project - 2/blackjack/src/utils/set_ending.py
@@ -0,0 +1,27 @@
+from src.utils.player import *
+from src.utils.card import *
+
+
+def set_ending(players):
+    """
+    Function to test if there is a winner in the game
+    Inputs : List of Player
+    Outputs : List of Player updated
+    """
+    croupier = players[0]
+    if croupier.game_value() > 21:
+        # All the players have won
+        players = map(lambda player.balance: player.balance+1.5*player.mise, players)
+    elif croupier.game_value() == 21:
+        # All the players have lost
+        players = map(lambda player.balance: max(0, player.balance-player.mise), players)
+    elif croupier.game_value() > 17:
+        for player in players[1:]:
+            if player.game_value() > croupier.game_value():
+                # Player with a bigger balance than croupier won
+                player.balance += 1.5*player.mise
+            elif player.game_value() > croupier.game_value():
+                # Player with lower balance than croupier lost
+                player.balance -= player.mise
+
+    return players
